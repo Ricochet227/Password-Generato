47daaffe234bc6d518feee9fabfff35f9a64f4d3
Ricochet227 <skyler7crawford@gmail.com>
2023-09-08 18:57:20 -0600

    Added rest of if statements
    Added the rest of the if statements to choose which characters you'd like in your password

--- Develop/script.js
+++ Develop/script.js
@@ -11,11 +11,13 @@ var specialCharArr =["!","#","$","%","&","*","+","/","<","=",">","?","@","^","~"
 
 // Write password to the #password input
 function writePassword() {
+  var correctPrompts = getPrompts();
+  if (correctPrompts){
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
+  }
 }
 
 // Add event listener to generate button
@@ -26,9 +28,23 @@ function generatePassword() {
 }
 
 function getPrompts(){
+  choiceArr = [];
   characterLength = parseInt(prompt("Pick any number between 8-128 characters for your password length"));
   if(isNaN(characterLength) (characterLength < 8 ) (characterLength > 128)) {
     alert("Your password has to be between 8-128 characters, try again!");
     return false;
   }
+  if(confirm("Do you want uppercasecase letters in your password? (If no press cancel)")){
+    choiceArr = choiceArr.concat(upperCaseArr);
+  }
+  if(confirm("Do you want lowercase letters in your password? (If no press cancel)")){
+    choiceArr = choiceArr.concat(lowerCaseArr);
+  }
+  if(confirm("Do you want numbers in your password? (If no press cancel)")){
+    choiceArr = choiceArr.concat(numberArr);
+  }
+  if(confirm("Do you want special characters in your password? (If no press cancel)")){
+    choiceArr = choiceArr.concat(specialCharArr);
+  }
+  return true;
 }
